81be4eaae1b222b2cbb0a9ef76d3d2109f587fe0
kularos <ian@kularos.com>
2022-04-20 20:41:17 -0400

    Introducing Terminal class

--- Application/live_plot.py
+++ Application/live_plot.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import collections
+from math import sin
 from Core import LiveTerminal, BufferedParameter
-
 from time import sleep
-
+import matplotlib
+matplotlib.use("GTKAgg")
 
 class Plotter(LiveTerminal):
     buffer_size = 200
+    t = BufferedParameter(size=buffer_size)
     x = BufferedParameter(size=buffer_size)
 
     def __init__(self):
@@ -16,29 +17,26 @@ class Plotter(LiveTerminal):
         plt.ion()
         self._fig = plt.figure()
         self._ax = self._fig.add_subplot(111)
-        self.line = self._ax.plot(np.array(self.t_), np.array(self.x_))[0]
-
+        self.line = self._ax.plot(np.array(self.t_), np.array(self.x_))
         # xlims are dynamically set
-        plt.xlim([0, self.dt * (self.buffer_size - 1)])
-        plt.ylim([0, 100])
-        plt.show()
+        plt.xlim([0, 100])
+        plt.ylim([-1, 1])
+        #plt.show()
 
         super(Plotter, self).__init__()
 
     def _update(self):
-        self.line.set_xdata(np.array(self.t_))
-        self.line.set_ydata(np.array(self.x_))
-        plt.show()
+
+        self.line[0].set_data(np.array(self.t_), np.array(self.x_))
+        self._fig.canvas.draw()
         super(Plotter, self)._update()
 
-    def add_value(self, value):
-        self._y.append(value)
-        self._update()
 
 
 if __name__ == '__main__':
     term = Plotter()
 
     while True:
-        print(term("get, t_"))
-        sleep(0.5)
+        t = term("get, t")
+        term.set('x', sin(t))
+        sleep(0.005)
